7d490051cfce2dc4e43ecd64d9fda8a4990d1ae7
Jaskaran Singh <snak3codes@outlook.com>
2020-07-16 19:07:21 -0400

    Add files via upload

---
+++ src/Fruit.cpp
@@ -0,0 +1,8 @@
+#include "include/Fruit.h"
+#include <iostream>
+
+using namespace std;
+
+Fruit::Fruit() {
+    cout << "I love #{FRUIT}!" << endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include "include/Fruit.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "New C++ Project! Hello #{NAME}!" << endl;
+    Fruit favoriteFruit;
+}
